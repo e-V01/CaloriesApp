1e04baccf25777e720d6354c75d734139024b6c0
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-12-04 12:14:02 +0200

    Initial Commit

---
+++ CaloriesApp/CaloriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesAppApp.swift
+//  CaloriesApp
+//
+//  Created by Y K on 04.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
